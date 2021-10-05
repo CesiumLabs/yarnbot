41240a263dd09ed3e3e81781e30e13a872949d9c
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-05 22:54:13 +0545

    chore: prettier

--- src/events/InteractionEvent.ts
+++ src/events/InteractionEvent.ts
@@ -15,32 +15,34 @@ export default class extends BaseEvent {
     async execute(interaction: Interaction) {
         if (interaction.isCommand() || interaction.isContextMenu()) {
             const command = this.commands.get(interaction.commandName);
-            
+
             try {
                 if (command) return await command.execute(interaction);
-            } catch(err) {
+            } catch (err) {
                 const error = err as Error;
-                if (!interaction.replied) return await interaction.reply({
-                    content: "Oh no! Something went wrong while executing this command:",
-                    embeds: [
-                        {
-                            title: "Command Error",
-                            color: COLORS.DANGER,
-                            description: error.stack || error.message || `${error}`,
-                            timestamp: Date.now(),
-                            footer: {
-                                text: `Command: ${interaction.commandName}`
+                if (!interaction.replied)
+                    return await interaction.reply({
+                        content: "Oh no! Something went wrong while executing this command:",
+                        embeds: [
+                            {
+                                title: "Command Error",
+                                color: COLORS.DANGER,
+                                description: error.stack || error.message || `${error}`,
+                                timestamp: Date.now(),
+                                footer: {
+                                    text: `Command: ${interaction.commandName}`
+                                }
                             }
-                        }
-                    ],
-                    ephemeral: true
-                });
+                        ],
+                        ephemeral: true
+                    });
             }
 
-            if (!interaction.replied) return await interaction.reply({
-                content: "Oh no! Looks like something is not right here!",
-                ephemeral: true
-            });
+            if (!interaction.replied)
+                return await interaction.reply({
+                    content: "Oh no! Looks like something is not right here!",
+                    ephemeral: true
+                });
         }
     }
 }
