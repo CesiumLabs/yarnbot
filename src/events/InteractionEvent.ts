9b7d193ba4d1ea351aaee2f4f1a57ab8ca05437f
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-05 22:53:55 +0545

    feat(InteractionEvent): add error handling

--- src/events/InteractionEvent.ts
+++ src/events/InteractionEvent.ts
@@ -1,6 +1,6 @@
 import { Collection, Constants, Interaction } from "discord.js";
 import { inject, injectable } from "tsyringe";
-import { CommandsToken } from "../Constants";
+import { COLORS, CommandsToken } from "../Constants";
 import BaseCommand from "../utils/BaseCommand";
 import BaseEvent from "../utils/BaseEvent";
 
@@ -15,9 +15,29 @@ export default class extends BaseEvent {
     async execute(interaction: Interaction) {
         if (interaction.isCommand() || interaction.isContextMenu()) {
             const command = this.commands.get(interaction.commandName);
-            if (command) return await command.execute(interaction);
+            
+            try {
+                if (command) return await command.execute(interaction);
+            } catch(err) {
+                const error = err as Error;
+                if (!interaction.replied) return await interaction.reply({
+                    content: "Oh no! Something went wrong while executing this command:",
+                    embeds: [
+                        {
+                            title: "Command Error",
+                            color: COLORS.DANGER,
+                            description: error.stack || error.message || `${error}`,
+                            timestamp: Date.now(),
+                            footer: {
+                                text: `Command: ${interaction.commandName}`
+                            }
+                        }
+                    ],
+                    ephemeral: true
+                });
+            }
 
-            return await interaction.reply({
+            if (!interaction.replied) return await interaction.reply({
                 content: "Oh no! Looks like something is not right here!",
                 ephemeral: true
             });
