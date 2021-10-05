563bf2c15bbf58e77da8c0f5e019317696cd387c
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-05 22:56:25 +0545

    refactor(Constants): make color value integer

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,11 +2,11 @@ export const DatabaseToken = Symbol("Database");
 export const ClientToken = Symbol("Client");
 export const CommandsToken = Symbol("Commands");
 export enum COLORS {
-    NORMAL = "#5865F2",
-    DANGER = "#ED4245",
-    SUCCESS = "#57F287",
-    WARNING = "#FEE75C",
-    LOVELY = "#EB459E",
-    LIGHT = "#FFFFFF",
-    DARK = "#000000"
+    NORMAL = 0x5865F2,
+    DANGER = 0xED4245,
+    SUCCESS = 0x57F287,
+    WARNING = 0xFEE75C,
+    LOVELY = 0xEB459E,
+    LIGHT = 0xFFFFFF,
+    DARK = 0x000000
 }
