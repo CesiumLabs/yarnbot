d56eed4990767a05acf03d77e23b8f902f61562c
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-05 22:56:48 +0545

    chore: prettier

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,11 +2,11 @@ export const DatabaseToken = Symbol("Database");
 export const ClientToken = Symbol("Client");
 export const CommandsToken = Symbol("Commands");
 export enum COLORS {
-    NORMAL = 0x5865F2,
-    DANGER = 0xED4245,
-    SUCCESS = 0x57F287,
-    WARNING = 0xFEE75C,
-    LOVELY = 0xEB459E,
-    LIGHT = 0xFFFFFF,
+    NORMAL = 0x5865f2,
+    DANGER = 0xed4245,
+    SUCCESS = 0x57f287,
+    WARNING = 0xfee75c,
+    LOVELY = 0xeb459e,
+    LIGHT = 0xffffff,
     DARK = 0x000000
 }
